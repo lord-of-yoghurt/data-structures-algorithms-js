46c163286e9a6185ae3ee73eb857cf3a44adeff4
David Ordovskiy <david.ordovsky@gmail.com>
2020-05-13 19:02:53 -0400

    add DLL push method and tests

--- 07-data-structures/01-doubly-linked-list/00-dll.js
+++ 07-data-structures/01-doubly-linked-list/00-dll.js
@@ -7,6 +7,36 @@ class DoublyLinkedList {
   constructor() {
     this.first = null;
     this.last = null;
+    this.length = 0;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  push(data) {
+    const newNode = new Node(data);
+
+    // if list empty, new node
+    // becomes first and last
+    if (this.isEmpty()) {
+      this.first = newNode;
+      this.last = newNode;
+    } 
+
+    // otherwise, point the pointers
+    else {
+      // last's next becomes the new node
+      this.last.next = newNode;
+      // new node's previous becomes current last
+      newNode.prev = this.last;
+      // last gets reassigned to the new node
+      this.last = newNode;
+    }
+
+    this.length++;
+
+    return this;
   }
 }
 
--- 07-data-structures/01-doubly-linked-list/test/00-dll.test.js
+++ 07-data-structures/01-doubly-linked-list/test/00-dll.test.js
@@ -18,3 +18,33 @@ describe('a node', () => {
     assert.equal(null, testNode.prev);
   });
 });
+
+describe('a doubly linked list', () => {
+  it('knows its first node', () => {
+    assert.equal(null, testList.first);
+  });
+
+  it('knows its last node', () => {
+    assert.equal(null, testList.last);
+  });
+
+  it('has a length', () => {
+    assert.equal(0, testList.length);
+  });
+
+  describe('push', () => {
+    it('handles an empty list', () => {
+      testList.push('apples');
+      console.log(testList);
+      assert.equal('apples', testList.first.data);
+      assert.equal('apples', testList.last.data);
+    });
+
+    it('adds a new node to the end of a list', () => {
+      testList.push('oranges');
+      console.log(testList);
+      assert.equal('apples', testList.first.data);
+      assert.equal('oranges', testList.last.data);
+    });
+  });
+});
