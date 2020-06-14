1d28a1086e9ab140f35d72d33e8f6f63991bf6a5
David Ordovskiy <david.ordovsky@gmail.com>
2020-06-14 19:04:01 -0400

    add heap insert method and tests

--- 07-data-structures/05-heaps/00-maxBinaryHeap.js
+++ 07-data-structures/05-heaps/00-maxBinaryHeap.js
@@ -2,6 +2,44 @@ class MaxBinaryHeap {
   constructor() {
     this.values = [];
   }
+
+  swapVal(idx1, idx2) {
+    if (!this.values[idx1] || !this.values[idx2]) {
+      throw new Error('Must provide indices for existing values!');
+    }
+
+    const temp = this.values[idx1];
+    this.values[idx1] = this.values[idx2];
+    this.values[idx2] = temp;
+  }
+
+  insert(val) {
+    const VALS = this.values;
+    // push the value to the end of the 
+    // values array
+    VALS.push(val);
+
+    // grab its index
+    let vIdx = VALS.length - 1;
+
+    // grab the index of its parent using formula
+    let pIdx = Math.floor((vIdx - 1) / 2);
+
+    // assuming the newly added value is larger
+    // than its parent's value, bubble it up
+    while (VALS[vIdx] > VALS[pIdx]) {
+      // swap them
+      this.swapVal(vIdx, pIdx);
+
+      // new value's index is now the parent index
+      vIdx = pIdx;
+
+      // update the parent index
+      pIdx = Math.floor((vIdx - 1) / 2);
+    }
+
+    return this;
+  }
 }
 
 module.exports = MaxBinaryHeap;
--- 07-data-structures/05-heaps/test/00-maxBinaryHeap.test.js
+++ 07-data-structures/05-heaps/test/00-maxBinaryHeap.test.js
@@ -12,4 +12,27 @@ describe('a max binary heap', () => {
   it('has an array of values as its only property', () => {
     assert.equal(true, testHeap.values instanceof Array);
   });
+
+
+  /*
+          70
+      40      30
+    10  15  25  20
+  */
+  describe('insert', () => {
+    it('inserts a value into the heap at the right place', () => {
+      
+      testHeap
+        .insert(40)
+        .insert(10)
+        .insert(25)
+        .insert(70)
+        .insert(15)
+        .insert(30)
+        .insert(20);
+
+      assert.equal(70, testHeap.values[0]);
+      assert.equal(20, testHeap.values[6]);
+    });
+  });
 });
