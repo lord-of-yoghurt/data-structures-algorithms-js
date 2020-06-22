02bf82c26fbbd35cedf627cb6ade782feccb86a6
David Ordovskiy <david.ordovsky@gmail.com>
2020-06-22 17:48:10 -0400

    add set method to hash table and tests

--- 07-data-structures/06-hash-table/hashTable.js
+++ 07-data-structures/06-hash-table/hashTable.js
@@ -1,8 +1,34 @@
 const hash = require('./hash');
 
 class HashTable {
+  // we need a prime number of elements in keyMap.
+  // default is 53, but it can be larger
   constructor(size = 53) {
     this.keyMap = new Array(size);
+  }
+
+  set(key, val) {
+    if (typeof key !== 'string' || typeof val !== 'string') {
+      return undefined;
+    }
+
+    // hash the key
+    const idx = hash(key, this.keyMap.length);
+
+    // use separate chaining to store the value.
+    // if there's nothing at this index,
+    if (!this.keyMap[idx]) {
+      // create an array and store the k-v pair there
+      // in their own sub-array
+      this.keyMap[idx] = [[key, val]];
+    } else {
+      // otherwise, if there's already an array at that index,
+      // we have a collision. push the new pair into the 
+      // existing array
+      this.keyMap[idx].push([key, val]);
+    }
+
+    return [key, val];
   }
 }
 
--- 07-data-structures/06-hash-table/test/hashTable.test.js
+++ 07-data-structures/06-hash-table/test/hashTable.test.js
@@ -8,4 +8,22 @@ describe('a hash table', () => {
   it('has a keyMap array with default size of 53', () => {
     assert.equal(53, testTable.keyMap.length);
   });
+
+  describe('set', () => {
+    it('stores a value using a hashed key', () => {
+      testTable.set('royal blue', '#4169e1');
+
+      const newPair = testTable.keyMap.find(val => !!val);
+
+      assert.equal('#4169e1', newPair[0][1]);
+    });
+
+    it('handles a collision using separate chaining', () => {
+      testTable.set('royal blue', '4169E1');
+
+      const pairs = testTable.keyMap.find(val => !!val);
+
+      assert.equal(2, pairs.length);
+    });
+  });
 });
